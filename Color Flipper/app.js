ef8017584f93b29ed5ad91b2392191ee402c3210
Vaishnav <vaishnavshyam33@gmail.com>
2021-07-27 11:46:42 +0530

    fix

--- Color Flipper/app.js
+++ Color Flipper/app.js
@@ -4,13 +4,11 @@ const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 
-
 btn.addEventListener('click', function() {
     let randomNumber = getRandomNumber();
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
-
 
 
 function getRandomNumber() {
